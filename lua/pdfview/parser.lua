bf73e48b82d844466ff1e2e5c2cef25853866e83
Basel Mahmoud <basel.mahmoud@arkleap.com>
2024-10-28 18:56:09 +0300

    fix

--- lua/pdfview/parser.lua
+++ lua/pdfview/parser.lua
@@ -1,5 +1,4 @@
 local M = {}
-local builtin = require('telescope.builtin')
 
 -- Function to extract text from PDF using pdftotext
 function M.extract_text(pdf_path)
@@ -24,33 +23,4 @@ function M.extract_text(pdf_path)
   return result
 end
 
--- Function to open Telescope and select a PDF file
-function M.select_pdf()
-  builtin.find_files({
-    prompt_title = "Select PDF",
-    cwd = vim.fn.getcwd(), -- Start in the current directory
-    find_command = { 'find', '.', '-type', 'f', '-name', '*.pdf' }, -- Restrict to PDF files
-    attach_mappings = function(prompt_bufnr, map)
-      local actions = require('telescope.actions')
-      local action_state = require('telescope.actions.state')
-
-      -- On selection of a file, pass the file to extract_text
-      map('i', '<CR>', function()
-        local selection = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
-
-        -- Call extract_text with the selected PDF file
-        local pdf_path = selection.path or selection[1]
-        local result = M.extract_text(pdf_path)
-        if result then
-          vim.api.nvim_out_write(result)
-        end
-      end)
-
-      return true
-    end
-  })
-end
-
 return M
-
